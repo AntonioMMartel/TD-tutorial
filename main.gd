03cd776b7ad0194df38a4145deb8684c2c3b8ca0
Antonio ML <AntonioMMartel@gmail.com>
2023-12-16 21:46:21 +0000

    Implemented raycast

--- main.gd
+++ main.gd
@@ -3,15 +3,37 @@ extends Node3D
 @export var path_corner:PackedScene # Will be rotated
 @export var path_straight:PackedScene
 @export var path_start:PackedScene
-@export var tile_empty:PackedScene
+@export var misc_tiles:Array[PackedScene]
 @export var path_crossing:PackedScene
 @export var basic_enemy:PackedScene
+
+@onready var camera = $Camera3D
+const RAYCAST_LENGTH: float = 100
 
 func _ready():
 	_display_path(_create_path())
 	_complete_grid()
 	spawn_enemies()
-	
+
+func _physics_process(delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		# Espacio 3D del mundo (Viewport)
+		var space_state = get_world_3d().direct_space_state
+		# Posicion del raton
+		var mouse_pos:Vector2 = get_viewport().get_mouse_position()
+		# El rayo aparece en origin (sacado en funcion de la perspectiva de la camara)
+		var origin:Vector3 = camera.project_ray_origin(mouse_pos)
+		# Y llega hasta end (en funcion de la perspectiva de la camara)
+		var end:Vector3 = origin + camera.project_ray_normal(mouse_pos) * RAYCAST_LENGTH
+		# Creas un rayo que vaya de origin a end
+		var query = PhysicsRayQueryParameters3D.create(origin, end)
+		# Que choque con cosas
+		query.collide_with_areas = true
+		# Invoca el rayo
+		var rayResult:Dictionary = space_state.intersect_ray(query)
+		if rayResult.size() > 0:
+			var co:CollisionObject3D = rayResult.get("collider")
+			print(co.get_groups())
 
 func _create_path() -> Array[Vector2i]:
 	return PathGeneratorSingleton.get_path_route()
@@ -32,10 +54,10 @@ func _complete_grid():
 	for x in range(PathGeneratorSingleton.get_map_length()):
 		for y in range(PathGeneratorSingleton.get_map_height()):
 			if not PathGeneratorSingleton.get_path_route().has(Vector2i(x,y)):
-				var tile:Node3D = tile_empty.instantiate()
-				
+				var tile:Node3D = misc_tiles.pick_random().instantiate()
 				add_child(tile)
 				tile.global_position = Vector3(x, 0, y)
+				tile.global_rotation_degrees = Vector3(0, randi_range(0,3)*90, 0)
 
 func _display_path(_path:Array[Vector2i]):
 	
@@ -76,13 +98,8 @@ func _display_path(_path:Array[Vector2i]):
 		if tile_rotation_score == ["Right"]:
 			tile = path_start.instantiate()
 			tile_rotation = Vector3(0, 270, 0)
-
 			
 		add_child(tile)
 		tile.global_position = Vector3(element.x, 0, element.y)
 		tile.global_rotation_degrees = tile_rotation
-		
-
-		
-		
 		
